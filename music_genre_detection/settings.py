7f2e8d0ea8cb1ac069c39bb2b16720bec981b4d0
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2022-04-17 14:06:27 +0530

    minor changes

--- music_genre_detection/settings.py
+++ music_genre_detection/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'music_genre_detection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
