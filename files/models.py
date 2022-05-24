a46d1e8eff6ddd6c01095eae18f495b862a45863
shubhganatra <shubhv611@gmail.com>
2022-05-24 23:12:40 +0530

    audio name

--- files/models.py
+++ files/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-
+import os
+from django.db import models
+from datetime import datetime
 # Create your models here.
 class materials(models.Model):
-    title = models.CharField(max_length = 100)
-    files = models.FileField(upload_to='')
 
-    def __str__(self):
+    def rename(path):
+        def wrapper(instance, filename):
+            d=datetime.now().strftime("%Y%m%d%H%M%S")
+            filename = f"{d}.wav"
+            return os.path.join(path,filename)
+        return wrapper
+        
+    title = models.CharField(max_length = 100)
+    files = models.FileField(upload_to=rename(''))
+
+    def str(self):
         return self.title
